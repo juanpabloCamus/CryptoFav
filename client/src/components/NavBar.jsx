85a518a21c90e9533eb49583c17d2105c129f473
Juan Pablo <juanpablocamus28@gmail.com>
2022-10-01 11:43:46 -0300

    adding Navbar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import './NavBar.css'
+
+function NavBar() {
+    return (
+        <div className='navbarContainer'>
+            <h1>CryptoFav</h1>
+            <span>List your favorites cryptos</span>
+        </div>
+    );
+}
+
+export default NavBar;
