85f357a6f2c27af7ceb5244fdcc7cc638089eebb
Juan Pablo <juanpablocamus28@gmail.com>
2022-10-01 23:44:22 -0300

    adding search funcionality

--- client/src/components/Searcher/Searcher.jsx
+++ client/src/components/Searcher/Searcher.jsx
@@ -3,22 +3,50 @@ import './Searcher.css';
 import axios from 'axios';
 import { CryptoCard } from '../CryptoCard/CryptoCard';
 
-//const requiredCryptos = ['btc', 'eth', 'ltc', 'trx']
+const requiredCryptos = ['btc', 'eth', 'ltc', 'trx']
 
 function Searcher({openDetail, closeSearcher, setDetail}) {
 
-    const [initialData, setInitalData] = useState([])
+    const [initialData, setInitalData] = useState([]);
+    const [search, setSearch] = useState('');
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         async function getData() {
-            let { data } = await axios.get(`https://api.cryptapi.io/btc/info/`);
-            setInitalData([...initialData, data])
+            // let { data } = await axios.get(`https://api.cryptapi.io/btc/info/`);
+            // setInitalData([...initialData, data])
+
+            requiredCryptos.map(async c => {
+                let { data } = await axios.get(`https://api.cryptapi.io/${c}/info/`);
+                initialData.push(data)
+                setInitalData(initialData)
+            })
         }
         getData()
     }, [])
 
+    const handleChange = (e) => {
+        setSearch(e.target.value)
+    }
+
+    const handleSearch = async (e) => {
+        e.preventDefault()
+        try{
+            let { data } = await axios.get(`https://api.cryptapi.io/${search}/info/`)
+            setInitalData([data])
+            setError(false)
+        }catch(e){
+            setError(true)
+        }
+    }
+
     return (
         <div className='searcher-container'>
+            <form onSubmit={handleSearch}>
+                <input onChange={handleChange} value={search}></input>
+                <button type='submit'>Buscar</button>
+            </form>
+            {error ? <h4>Busqueda invalida</h4> : null}
             {
                 initialData.map(c => 
                     (
